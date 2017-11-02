9bb0d23415a2ed02c0381851a0332a76891d3a27
Glarity <tolson519@gmail.com>
2017-11-01 21:59:14 -0700

    Fixed buttons, added sections, improved work panels

--- js/parallax.js
+++ js/parallax.js
@@ -11,20 +11,16 @@ function onScroll() {
 
 	for (i = 0; i < parallax.length; i++) {
 		parallax[i].style.backgroundPosition = '0px ' + -currScrollPos / 3 + 'px';
-		parallax[i].style.zIndex = -2;
 	}
 	for (i = 0; i < parallaxCenter.length; i++) {
 		parallaxCenter[i].style.backgroundPosition = 'center ' + -currScrollPos / 3 + 'px';
-		parallaxCenter[i].style.zIndex = -2;
 
 	}
 	for (i = 0; i < parallaxText.length; i++) {
 		parallaxText[i].style.transform = 'translate(0px, ' + currScrollPos / 3 + '%)';
-		parallaxText[i].style.zIndex = -1;
 
 	}
 	for (i = 0; i < parallaxTextCenter.length; i++) {
 		parallaxTextCenter[i].style.transform = 'translate(-50%, ' + (-50 + (currScrollPos / 3)) + '%)';
-		parallaxTextCenter[i].style.zIndex = -1;
 	}
 }
