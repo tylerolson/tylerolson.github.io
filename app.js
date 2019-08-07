402493a70abcd60e3af28029b6f7b3a21d397553
Tyler Olson <tolson519@gmail.com>
2019-08-06 17:10:27 -0700

    Start script for Heroku

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
-app.listen(8080, function(){
+app.listen(80, function(){
     console.log("Running Express server.")
 });
